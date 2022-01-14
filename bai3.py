8cb3f6e64181c039a28f35b3ebbb1bfdcbcde0d5
tuananhuuu <90943165+tuananhuuu@users.noreply.github.com>
2022-01-14 10:56:44 +0700

    Add files via upload

---
+++ bai3.py
@@ -0,0 +1,17 @@
+# Lò Tuấn Anh
+# msv:1451020005
+ten = input(" Nhap ten cua ban: ")
+print(ten)
+n = [];
+def thuan_nghich(n):
+    m = n
+    a = []
+    while n!= 0:
+        a.append(n % 10)
+        n = n // 10
+    a = ''.join(str(x) for x in a)
+    if a == m:
+        print(m, "la so thuan nghich")
+    else:
+        print(m, "khong la so thuan nghich")
+thuan_nghich(1225)
